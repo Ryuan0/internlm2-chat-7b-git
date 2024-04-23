419ccce2a397162b6bf2c4916a27b79a892810a0
Ryuan0 <127178561+Ryuan0@users.noreply.github.com>
2024-04-23 13:48:17 +0800

    Update app.py
    change the path to "master" by adding the comments" git clone -b master" ;

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, AutoModel
 # download internlm2 to the base_path directory using git tool
 base_path = './InternLM2-Chat-7B'
 # os.system(f'git clone https://code.openxlab.org.cn/OpenLMLab/internlm2-chat-7b.git {base_path}')
-os.system(f'git clone https://code.openxlab.org.cn/RYUAN0/InternLM2-Chat-7B.git {base_path}')
+os.system(f'git clone -b master https://code.openxlab.org.cn/RYUAN0/InternLM2-Chat-7B.git {base_path}')
 os.system(f'cd {base_path} && git lfs pull')
 
 tokenizer = AutoTokenizer.from_pretrained(base_path,trust_remote_code=True)
